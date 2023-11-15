ef14d6e59b2ae54f7b45996f9ba63a8738c01217
joaotartari <joaotartari@outlook.com>
2023-11-15 19:27:43 -0300

    Atualizações

---
+++ Lista_2/script.js
@@ -0,0 +1,35 @@
+// Aqui serão adicionados seus script
+
+// step 1
+const noticiaPrincipal = document.getElementById('noticia-principal');
+alert(noticiaPrincipal.innerHTML);
+
+// step 2
+const h1 = document.getElementsByTagName('h1');
+for (let i = 0; i < h1.length; i++) {
+  h1[i].style.color = 'red';
+}
+
+// step 3
+const listaP = document.getElementsByClassName('paragrafo-noticia');
+
+for (let i = 0; i < listaP.length; i++) {
+  listaP[i].innerHTML = 'Leia a notícia completa';
+}
+
+// step 4
+const comentarios = document.getElementById('secao-comentarios');
+const p = document.createElement('p');
+p.innerHTML = 'Deixe seu comentário';
+comentarios.appendChild(p);
+
+// step 5
+const atualizacoes = document.getElementById('ultimas-atualizacoes');
+const filhosAtualizacoes = atualizacoes.children;
+atualizacoes.removeChild(filhosAtualizacoes[filhosAtualizacoes.length - 1]);
+
+//Desafio Extra
+const alterar = document.getElementById('alternar-tema');
+alterar.addEventListener('click', () => {
+  document.body.classList.toggle('temaEscuro');
+});
---
+++ Lista_3/script.js
@@ -0,0 +1,56 @@
+// Step 1
+function Esconder() {
+  const foto = document.getElementById('foto');
+  const botão = document.getElementById('Esconder/Mostrar');
+  const imagem = document.getElementById('imagem');
+  if (foto.style.visibility == 'hidden') {
+    foto.style.visibility = 'visible';
+    botão.innerHTML = 'Ocultar';
+    imagem.style.height = '300px';
+  } else {
+    foto.style.visibility = 'hidden';
+    botão.innerHTML = 'Mostrar';
+    imagem.style.height = '1px';
+  }
+}
+// step 2
+function alteraText() {
+  const div = document.getElementById('sobre');
+  const input1 = document.getElementById('input1');
+  div.style.textAlign = input1.value;
+}
+// step 3
+function alteraCor() {
+  const cabecalho = document.getElementById('header');
+  const rodape = document.getElementById('footer');
+  const input2 = document.getElementById('input2');
+  if (input2.value == 'black') {
+    cabecalho.style.backgroundColor = input2.value;
+    rodape.style.backgroundColor = input2.value;
+    cabecalho.style.color = 'white';
+    rodape.style.color = 'white';
+  } else if (input2.value == 'white') {
+    cabecalho.style.backgroundColor = input2.value;
+    rodape.style.backgroundColor = input2.value;
+    cabecalho.style.color = 'black';
+    rodape.style.color = 'black';
+  } else {
+    cabecalho.style.backgroundColor = input2.value;
+    rodape.style.backgroundColor = input2.value;
+  }
+}
+function alteraFont() {
+  const input3 = document.getElementById('input3');
+  const divSobre = document.getElementById('sobre');
+  divSobre.style.fontFamily = input3.value;
+}
+function alteraModo(tema) {
+  const conteudo = document.getElementById('conteudo');
+  if (tema == 'claro') {
+    conteudo.style.backgroundColor = 'white';
+    conteudo.style.color = 'black';
+  } else {
+    conteudo.style.backgroundColor = 'black';
+    conteudo.style.color = 'white';
+  }
+}
